1ec374f1473854137ded5bb4c29de5bb7f25b48f
jlawrenc3-ops <jlawrenc3@gmail.com>
2026-04-18 13:39:31 -0400

    #19 updated file manager with block and key structures

--- firmware/include/file_manager.h
+++ firmware/include/file_manager.h
@@ -6,12 +6,25 @@
  *
  * Wrapper functions for the file manager.
  */
+#include <stdalign.h>
+#include "file_manager.c"
 
-/* Needs to be 128-bit aligned */
- typedef struct {
-    // TODO: We need to fill this struct fields following the FM layout described in docs
+/* Blocks Layout, 128-bit aligned*/
+ typedef struct alignas(16) {
+      uint8_t status[4];       
+      uint8_t file_id[1];
+      uint8_t payload_size[1];
+      uint8_t reservedblock1[2];
+      uint8_t iv[12];
+      uint8_t auth_tag[16];
+      uint8_t reservedblock2[4];
+      uint8_t payload[88];  
  }fm_layout;
 
- typedef struct {
-    // TODO: We need to fill this struct fields following the FM layout described in docs
+/* Key Layout, 128-bit aligned */
+ typedef struct alignas(16) {
+      uint8_t key_id[4];
+      uint8_t key[32];
+      uint8_t reservedkey1[4];
+      uint8_t reservedkey2[40];
  }fm_key_layout;
