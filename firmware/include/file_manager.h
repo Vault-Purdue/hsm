484722e742a5c3adef0e51d97d22eb1ed3e96729
Alexander Avilez <aavilezb@purdue.edu>
2026-04-21 23:52:20 -0400

    fix key and files struct layout

--- firmware/include/file_manager.h
+++ firmware/include/file_manager.h
@@ -9,22 +9,26 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-/* Blocks Layout, 128-bit aligned*/
-typedef struct __attribute__((aligned(16))) {
-      uint32_t status;       
-      uint8_t file_id[1];
-      uint8_t reserved_align;
-      uint16_t payload_size;
-      uint8_t iv[12];
-      uint8_t auth_tag[16];
-      uint32_t reserved;
-      uint8_t payload[88];  
- }fm_layout;
+#define FLASH_BLOCK_SIZE 128
+#define FLASH_SECTOR_SIZE 1024
 
-/* Key Layout, 128-bit aligned */
-typedef struct __attribute__((aligned(16))) {
+/* Blocks Layout, 128-byte aligned*/
+typedef struct __attribute__((packed, aligned(16))) {
+    uint32_t status;
+    uint8_t  file_id;
+    uint8_t  reserved_0[3];
+    uint8_t  iv[12];
+    uint8_t  auth_tag[16];
+    uint16_t payload_size;
+    uint8_t  reserved_1[2];
+    uint8_t  payload[88];
+} fm_layout;
+
+/* Key Layout, 128-byte aligned */
+typedef struct __attribute__((packed, aligned(16))) {
       uint32_t key_id;
       uint8_t key[32];
-      uint32_t reservedkey1;
-      uint8_t reservedkey2[40];
+      uint8_t  iv[12];
+      uint8_t  auth_tag[16];
+      uint8_t reserved[64];
  }fm_key_layout;
